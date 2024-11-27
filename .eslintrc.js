aa14246bce91bcee5feec64346f788bdd8057034
arcodez <abelileitor123@gmail.com>
2024-11-26 21:24:32 -0500

    update eslint config file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,17 @@
 module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
   rules: {
     "react/no-unescaped-entities": "off",
   },
